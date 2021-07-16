45756263305fa9ded6c04b3cc051ba23a495a951
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-16 22:21:53 +0800

    updated recongizer to mitigate the effect of noise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
 import moviepy.editor as mp
 
-# Extracing the audio from the video
-clip = mp.VideoFileClip(r"animal.mp4")
+# Extracing the audio from a video in the same directory
+clip = mp.VideoFileClip(r"animals.mp4")
 clip.audio.write_audiofile(r"converted.wav")
 
 # Defining the recognizer and importing the audio file
@@ -11,13 +11,16 @@ audio = sr.AudioFile("converted.wav")
 
 # Recognizing the audio using Google Speech Recognization
 with audio as source:
+    r.adjust_for_ambient_noise(source)
     audio_file = r.record(source)
+
 result = r.recognize_google(audio_file)
 
 
 # Exporting the result
 with open('script.txt', mode='w') as file:
     file.write("Recongized Speech: ")
+    file.write("\n")
     file.write("\n")
     file.write(result)
     print("The script is ready!")
